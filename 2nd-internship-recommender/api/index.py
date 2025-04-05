97c2adf823d2828b283f3c1f792089e28baa3fb8
Virat-Ghatge <viratghatge71@gmail.com>
2025-04-05 19:06:08 +0530

    Add files via upload

---
+++ 2nd-internship-recommender/api/index.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from recommender import recommend_internships
+from flask_cors import CORS
+
+app = Flask(__name__, template_folder="../templates")
+CORS(app)
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+@app.route("/recommend", methods=["POST"])
+def recommend():
+    user_skills = request.form['skills']
+    recommendations = recommend_internships(user_skills)
+    return render_template("result.html", recommendations=recommendations)
+
+# Vercel handler
+def handler(event, context):
+    from mangum import Mangum
+    return Mangum(app)(event, context)
